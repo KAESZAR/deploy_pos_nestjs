89816d37f13eb3b7a5f1823d8a0aaaa8122567b1
KAESZAR <cesdev.s@outlook.com>
2026-09-11 11:37:47 -0300

    fix: ssl config and connection timeout for render deploy

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -4,22 +4,23 @@ import { join } from "path"
 
 export const typeOrmConfig = (configService: ConfigService) : TypeOrmModuleOptions => ({
     type: 'postgres',
-    host: configService.get('DATABASE_HOST'), // accediendo a variables de entorno
+    host: configService.get('DATABASE_HOST'),
     port: configService.get('DATABASE_PORT'),
     username: configService.get('DATABASE_USER'),
     password: configService.get('DATABASE_PASS'),
     database: configService.get('DATABASE_NAME'),
-    ssl: true,
+    ssl: {
+        rejectUnauthorized: false,
+    },
     logging: true,
-    entities: [join(__dirname + '../../**/*.entity.{js,ts}')],//recurcividad para buscar entidades
-    synchronize: true // solo utilizar en desarrollo
-    
-    //synchronize: configService.get('NODE_ENV') !== 'production' 
-    //automaticamente deshabilitar synchronize en produccion. Tener NODE_ENV=production en tus variables de entorno del servidor.
+    entities: [join(__dirname + '../../**/*.entity.{js,ts}')],
+    synchronize: configService.get('NODE_ENV') !== 'production',
+    extra: {
+        connectionTimeoutMillis: 30000,
+    },
 })
 
 
 // dependencias para variables de entorno: npm i @nestjs/config
-
 
 
